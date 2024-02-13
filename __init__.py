e4c5ad4a41ad9e29f48f69be89f6cc9074d619fe
sabri045 <159913943+sabri045@users.noreply.github.com>
2024-02-13 15:24:23 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -39,22 +39,9 @@ def get_paris_weather():
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
-@app.route('/commits/')
-def get_commit_minutes():
-    # URL de l'API GitHub pour les commits du projet
-    url = "https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits"
-
-    # Récupération des données depuis l'API GitHub
-    response = requests.get(url)
-    commits_data = response.json()
-
-    # Extraction des minutes de chaque commit
-    commits_minutes = [extract_minutes(commit['commit']['author']['date']) for commit in commits_data]
-
-    # Création du format JSON avec les minutes des commits
-    results = [{'minute': minute} for minute in commits_minutes]
-    
-    return jsonify(results=results)
+@app.route("/commits/")
+def commits():
+    return render_template("commits.html")
     
 @app.route("/histogramme/")
 def monhisto():
