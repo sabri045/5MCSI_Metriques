2e5bcfdcc80ec93fd75d977e48158acefb968fc6
sabri045 <159913943+sabri045@users.noreply.github.com>
2024-02-13 14:48:44 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -27,6 +27,19 @@ def MaPremiereAPI():
 def mongraphique():
     return render_template("graphique.html")
 
+@app.route('/paris/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=48.8566&lon=2.3522&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
+
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #commentaire
